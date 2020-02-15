aa17536a86bbb326906d7b210c56cb7f1fc64e51
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2020-02-15 08:09:28 +0300

    reduce bundle size

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, lazy } from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import Layout from "./Layouts/Layout";
-import Home from "./Layouts/Pages/Home";
-import Blog from "./Layouts/Pages/Blog";
-import About from "./Layouts/Pages/About";
-import Service from "./Layouts/Pages/Services";
-import Contact from "./Layouts/Pages/Contact";
-import SinglePost from "./Sections/SinglePost";
-import ErrorPage from "./Components/Error";
+import Layout from "./Layouts/Layout"
 import "./css/style.css";
 import "./css/main.css";
 
+const Home = lazy(() => import("./Layouts/Pages/Home"));
+const Blog = lazy(() => import("./Layouts/Pages/Blog"));
+const About = lazy(() => import("./Layouts/Pages/About"));
+const Service = lazy(() => import("./Layouts/Pages/Services"));
+const Contact = lazy(() => import("./Layouts/Pages/Contact"));
+const SinglePost = lazy(() => import("./Sections/SinglePost"));
+const ErrorPage = lazy(() => import("./Components/Error"));
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Layout>
-            <Switch>
-              <Route path="/" component={Home} exact />
-              <Route path="/about" component={About} />{" "}
-              <Route path="/services" component={Service} exact />
-              <Route path="/blog" exact component={Blog} />{" "}
-              <Route path="/blog/:id" component={SinglePost} />{" "}
-              <Route path="/contact" component={Contact} />{" "}
-              <Route path="*" component={ErrorPage} />{" "}
-            </Switch>{" "}
+            <Suspense fallback={<div>Loading...</div>}>
+              <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/about" component={About} />{" "}
+                <Route path="/services" component={Service} exact />
+                <Route path="/blog" exact component={Blog} />{" "}
+                <Route path="/blog/:id" component={SinglePost} />{" "}
+                <Route path="/contact" component={Contact} />{" "}
+                <Route path="*" component={ErrorPage} />{" "}
+              </Switch>{" "}
+            </Suspense>
           </Layout>{" "}
         </div>{" "}
       </BrowserRouter>
